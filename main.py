bc9c164d43cc3efa954120036fbe3c42e6dd9613
FilinMen <fiilinkadabra@tutamail.com>
2024-10-11 16:25:55 +0300

    Think easier

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 input_data = open("input.txt","r")
 data = input_data.read()
-data = list(data)
-c = (data[0]) + (data[1]) + (data[2])
-c = int(c)
-h = c + 1
-x = h * c 
-x = str(x)
-i = str(25)
-end = x + i
+data = int(data)
+a = data ** 2
+a = str(a)
 output_data = open("output.txt","w")
-output_data.write(end)
+output_data.write(a)
 input_data.close()
 output_data.close()
-
